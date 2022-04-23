3a98eb6810a471647df4afdb41d163e57fae54cf
afinanding <61938587+afinanding@users.noreply.github.com>
2022-04-23 15:48:52 +0200

    chore

--- src/client/Program.cs
+++ src/client/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using PBIXUnpacker;
+// See https://aka.ms/new-console-template for more information
+string pbixRoute = @"..\..\samples\pbix\sample.pbix";
+string outRoute = @"..\..\samples\pbix\";
+Unpacker u = new();
+u.UnpackPbix(pbixRoute: pbixRoute, outRoute: outRoute);
+Console.WriteLine("END");
---
+++ src/pbixunpacker/Unpacker.cs
@@ -0,0 +1,15 @@
+﻿using System.IO.Compression;
+
+namespace PBIXUnpacker;
+public class Unpacker
+{
+    public void UnpackPbix(string pbixRoute, string outRoute)
+    {
+        //Check if file exists
+        //Check permissions on outRoute
+        ZipFile.ExtractToDirectory(pbixRoute,outRoute);
+        //OpenFile
+            //unzip it
+            //Prettyfy it    
+    }
+}
